35b51909dd76214ec6b03548b915c20edac55ccf
natnael-eyuel-dev <natnael.eyuel.dev@gmail.com>
2025-07-31 10:11:38 +0300

    feat(auth): implement OAuth2 authentication with multiple provider - Add OAuth2 service with support for Google, GitHub, and Facebook - Create provider interface and implementations for each supported platform - Add configuration structure for OAuth2 provider credentials - Implement secure state management for OAuth2 flow - Handle user data normalization across different providers

---
+++ infrastructures/auth/providers/facebook.go
@@ -0,0 +1,105 @@
+package providers
+
+// imports
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/url"
+
+	"github.com/InkForge/Blog_Website/domain"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/facebook"
+)
+
+type facebookProvider struct {
+	config *oauth2.Config
+}
+
+// creates a new Facebook OAuth2 provider
+func NewFacebookProvider(cfg domain.OAuth2ProviderConfig) *facebookProvider {
+	if len(cfg.Scopes) == 0 {
+		cfg.Scopes = []string{"email", "public_profile"}
+	}
+
+	return &facebookProvider{
+		config: &oauth2.Config{
+			ClientID:     cfg.ClientID,
+			ClientSecret: cfg.ClientSecret,
+			RedirectURL:  cfg.RedirectURL,
+			Scopes:       cfg.Scopes,
+			Endpoint:     facebook.Endpoint,
+		},
+	}
+}
+
+func (fbprov *facebookProvider) Name() string {
+	return "facebook"
+}
+
+func (fbprov *facebookProvider) GetAuthorizationURL(state string) string {
+	return fbprov.config.AuthCodeURL(state, oauth2.AccessTypeOffline)
+}
+
+func (fbprov *facebookProvider) Authenticate(ctx context.Context, code string) (*domain.OAuth2User, error) {
+	
+	token, err := fbprov.config.Exchange(ctx, code)
+	if err != nil {
+		return nil, fmt.Errorf("facebook: code exchange failed: %w", err)
+	}
+
+	client := fbprov.config.Client(ctx, token)
+	
+	// facebook requires fields to be specified
+	profileURL := fmt.Sprintf(
+		"https://graph.facebook.com/me?fields=id,name,first_name,last_name,email,picture.type(large)&access_token=%s",
+		url.QueryEscape(token.AccessToken),
+	)
+
+	resp, err := client.Get(profileURL)
+	if err != nil {
+		return nil, fmt.Errorf("facebook: failed getting user info: %w", err)
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("facebook: failed reading response body: %w", err)
+	}
+
+	var userInfo struct {
+		ID        	string 	 `json:"id"`
+		Email     	string 	 `json:"email"`
+		Name      	string   `json:"name"`
+		FirstName 	string   `json:"first_name"`
+		LastName  	string   `json:"last_name"`
+		Picture   	struct {
+			Data 	struct {
+				URL string `json:"url"`
+			} `json:"data"`
+		} `json:"picture"`
+	}
+
+	if err := json.Unmarshal(data, &userInfo); err != nil {
+		return nil, fmt.Errorf("facebook: failed parsing user info: %w", err)
+	}
+
+	// parse raw data
+	var rawData map[string]interface{}
+	if err := json.Unmarshal(data, &rawData); err != nil {
+		rawData = make(map[string]interface{})
+	}
+
+	return &domain.OAuth2User{
+		ID:            userInfo.ID,
+		Email:         userInfo.Email,
+		VerifiedEmail: true,        // facebook doesn't provide this info
+		Name:          userInfo.Name,
+		FirstName:     userInfo.FirstName,
+		LastName:      userInfo.LastName,
+		Picture:       userInfo.Picture.Data.URL,
+		Provider:      fbprov.Name(),
+		RawData:       rawData,
+	}, nil
+}
