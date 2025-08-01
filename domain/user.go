fe8dbc0f071516567336a3323a87a89ee3298ee3
Enoch-Tadesse <henitadesse97@gmail.com>
2025-08-01 16:22:45 +0300

    implemented user repo, converting user model, improved user interface

--- domain/user.go
+++ domain/user.go
@@ -8,43 +8,45 @@ import (
 type Role string
 
 const (
-  RoleUser     Role = "USER"
-  RoleAdmin    Role = "ADMIN"
-  
+	RoleUser  Role = "USER"
+	RoleAdmin Role = "ADMIN"
 )
 
-
 type User struct {
-	UserID         	string
-	Name            *string      // for oauth2 user
-	Username        *string
-	FirstName       *string
-	LastName        *string
-	Bio             *string
-	ProfilePicture  *string
-	IsVerified 	 	bool
-	Email           string
-	Password        *string
-	RefreshToken    *string
-	AccessToken     *string
-	CreatedAt       time.Time
-	UpdatedAt       time.Time
+	UserID         string
+	Name           *string // for oauth2 user
+	Username       *string
+	FirstName      *string
+	LastName       *string
+	Bio            *string
+	ProfilePicture *string
+	IsVerified     bool
+	Email          string
+	Password       *string
+	RefreshToken   *string
+	AccessToken    *string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 
-	Provider        string      // for oauth2 user
-	RawData         map[string]interface{} 
+	Provider string // for oauth2 user
+	RawData  map[string]any
 
-	Role            Role
+	Role Role
 }
 
-
-//UserRepository Interface
-
+// UserRepository Interface
 type IUserRepository interface {
-	CreateUser(user User) error
-	FindByEmail(email string) (User, error)
-	CountByEmail(email string) (int64, error)
-	CountAll() (int64, error)
-	IsVerified(email string)bool
+	CreateUser(c context.Context, user *User) error
+	FindByEmail(c context.Context, email string) (*User, error)
+	FindByID(c context.Context, id string) (*User, error)
+	UpdateUser(c context.Context, user *User) error
+	DeleteByID(c context.Context, id string) error
+
+	SetEmailVerified(c context.Context, userID string) error
+	IsEmailVerified(c context.Context, userID string) (bool, error)
+
+	CountByEmail(c context.Context, email string) (int64, error)
+	CountAll(c context.Context) (int64, error)
 }
 
 //PasswordService Interface
@@ -57,23 +59,22 @@ type IPasswordService interface {
 //JWTService Interface
 
 type IJWTService interface {
-	GenerateVerificationToken(userID string)(string,error)
+	GenerateVerificationToken(userID string) (string, error)
 	GenerateAccessToken(userID string, role string) (string, error)
 	GenerateRefreshToken(userID string, role string) (string, error)
 	ValidateToken(token string, isRefresh bool) (string, error)
 }
 
 type OAuth2ProviderConfig struct {
-	ClientID       string
-	ClientSecret   string
-	RedirectURL    string
-	Scopes         []string
+	ClientID     string
+	ClientSecret string
+	RedirectURL  string
+	Scopes       []string
 }
 
 // OAuth2 providers interface
 type IOAuth2Provider interface {
-	
-	Name() string   // provider name
+	Name() string // provider name
 	Authenticate(ctx context.Context, code string) (*User, error)
 	GetAuthorizationURL(state string) string
 }
